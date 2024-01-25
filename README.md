12f0bf38a7a6bfee805408c4683f96869453beb8
logo4poop <logo4poop@protonmail.com>
2024-01-25 09:23:33 -0500

    Lets go!

---
+++ README.md
@@ -0,0 +1,17 @@
+# Swatch
+Extremely simple tool to convert time to swatch. It was a fun exercise to play with Haxe's date class!
+
+## Usage
+```
+swatch - converts time to swatch
+usage: swatch [time]
+Time parameter can be emitted for current time. It can be formatted as:
+    - "YYYY-MM-DD hh:mm:ss"
+    - "YYYY-MM-DD"
+    - "hh:mm:ss"
+```
+
+## Future plans
+Not sure if I have time for it right now, but turning this into some form of TUI clock would be real neat!
+
+It would also be fun to do this in an esoteric language like Haskell or Rust to see how they compare to Haxe. Originally I planned to write this in Zig but the date library seemed below par. Maybe now that I wrote it in Haxe it would be easier to translate.
---
+++ src/Main.hx
@@ -0,0 +1,60 @@
+package;
+
+class Main {
+	inline static var beatSeconds = 86.4;
+
+	static function main() {
+		final args = Sys.args();
+
+		var date: Date = Date.now();
+		if (args.length >= 1) {
+			if (args[1].toLowerCase() == "--help") {
+				help();
+				return;
+			}
+
+			try {
+				date = Date.fromString(args[1]);
+			} catch (_) {
+				Sys.println('swatch: unrecognized argument');
+				help();
+			}
+		};
+
+		// Display
+		if (date != null) {
+			Sys.println(getSwatch(date));
+		}
+	}
+
+	static function help(): Void {
+		var buf = new StringBuf();
+		buf.add("swatch - converts time to swatch internet time\n");
+		buf.add("usage: swatch [time]\n");
+		buf.add("Time parameter can be emitted for current time. It can be formatted as:\n");
+		buf.add("	- \"YYYY-MM-DD hh:mm:ss\"\n");
+		buf.add("	- \"YYYY-MM-DD\"\n");
+		buf.add("	- \"hh:mm:ss\"");
+		Sys.println(buf.toString());
+	}
+
+	/**
+	 * Converts Date -> Swatch
+	 */
+	public static function getSwatch(date: Date) {
+		final hours = date.getUTCHours() + 1 % 24;
+		final minutes = date.getUTCMinutes();
+		final seconds = date.getUTCSeconds();
+		final amountOfSeconds = (hours * 60 + minutes) * 60 + seconds;
+
+		return round(amountOfSeconds / beatSeconds, 2);
+	}
+
+	/**
+	 * Rounds to a specific decimal precision, borrowed from THX
+	 */
+	static function round(number: Float, precision: Int): Float {
+		number *= Math.pow(10, precision);
+		return Math.round(number) / Math.pow(10, precision);
+	}
+}
